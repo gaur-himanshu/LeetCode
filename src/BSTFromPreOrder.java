d04562c2a92e40ee58fd4d234c45723a4506896e
gaur-himanshu <himanshu.gaur_cs19@gla.ac.in>
2022-04-28 08:23:04 +0530

    leetcode 108

---
+++ src/BSTFromPreOrder.java
@@ -0,0 +1,44 @@
+public class BSTFromPreOrder {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    class Solution {
+        int nodeIdx;
+
+        public TreeNode bstFromPreorder(int[] preorder) {
+
+            if (preorder == null)
+                return null;
+            nodeIdx = 0;
+            return bstHelper(preorder, Integer.MIN_VALUE, Integer.MAX_VALUE);
+
+        }
+
+        private TreeNode bstHelper(int[] preOrder, int start, int end) {
+
+            if (nodeIdx == preOrder.length || preOrder[nodeIdx] < start || preOrder[nodeIdx] > end)
+                return null;
+            int val = preOrder[nodeIdx++];
+            TreeNode node = new TreeNode(val);
+            node.left = bstHelper(preOrder, start, val);
+            node.right = bstHelper(preOrder, val, end);
+            return node;
+        }
+    }
+}
